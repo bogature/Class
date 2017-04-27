2f81168b5f077344fc1653d2372c2d61e9168f11
Andrey <o.bogature@gmail.com>
2017-04-27 15:56:37 +0300

    created classes and interfaces

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,11 @@
-public class Main {
+import Run.BMV;
+import Run.Mazda;
 
+public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        Mazda A = new Mazda("Mashin1", 100, 2500, 2, 4, 8, "Bronia-12");
+        BMV B = new BMV("Mashin2", 120, 1000, 5, 4, "Pasagir");
+        System.out.println(A.toSting() + "\n");
+        System.out.println(B.toSting());
     }
 }
